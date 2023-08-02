9212881e929c31bce892a382b1209f7632e81653
erikkrn <erikkurniawann922@gmail.com>
2023-08-03 00:28:06 +0700

    fix navbar pembayaran

--- pembayaran.php
+++ pembayaran.php
@@ -38,29 +38,12 @@ if ($id_pelanggan_login !== $id_pelanggan_beli)
 <html>
 <head>
 	<title>Pembayaran</title>
-	<link rel="stylesheet" href="admin/assets/css/bootstrap.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="admin/assets/css/font1.css">
 </head>
 <body>
 	<!--Navbar-->
-	<nav class="navbar navbar-default">
-		<div class="container">
-
-			<ul class="nav navbar-nav">
-				<li><a href="index.php">Home</a></li>
-				<li><a href="keranjang.php">Keranjang</a></li>
-				<?php if (isset($_SESSION['pelanggan'])):?>
-				<li><a href="riwayat.php">Riwayat Belanja</a></li>
-				<li><a href="logout.php">Logout</a></li>
-
-				<?php else : ?>
-				<li><a href="login.php">Login</a></li>
-				<li><a href="daftar.php">Daftar</a></li>
-				<?php endif  ?>
-				
-				<li><a href="checkout.php">Checkout</a></li>
-			</ul>
-	</div>
-	</nav>
+	<?php include "menu.php" ?>
 
 	<div class="container">
 		<h2>Konfirmasi Pembayaran</h2>
